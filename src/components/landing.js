eaf0786c3e7daee8c2ef8bc06708586cc254fcb4
Gatsby <zacariealancarr@gmail.com>
2019-07-20 15:03:51 +1000

    Update landing.js

--- src/components/landing.js
+++ src/components/landing.js
@@ -25,16 +25,34 @@ const LandingContainer = styled.div `
         position: relative;
         height: 100%;
         width: 100%;
+    `}
 
-        }
+    /* Styles for medium screens */
+    ${media.between("medium", "large")`
+        height: 100%;
+        width: 70%;
+        background-color: green;
+    `}
+    
+    /* Styles for large screens */
+    ${media.greaterThan("large")`
+        height: 100%;
+        width: 100%;
+        background-color: green;
+    `}
+`;
 
-        .half {
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-            height: 500px;
-            width: 100%;
-            background-color: #4bffa5;
+const Half = styled.div `
+
+    /* Styles for small screens */
+    ${media.lessThan("medium")`
+        display: flex;
+        position: relative;
+        flex-direction: column;
+        justify-content: flex-end;
+        height: 411px;
+        width: 100%;
+        background-color: #4bffa5;
 
             h1 {
                 position: relative;
@@ -51,54 +69,27 @@ const LandingContainer = styled.div `
                 padding-left: 10px;
 
             }
-        }
-
-        .half-half {
-            display: flex;
-            flex-direction: column;
-            height: 500px;
-            width: 100%;
-            background-color: #ffffff;
-        }
     `}
 
     /* Styles for medium screens */
     ${media.between("medium", "large")`
-        height: 100%;
-        width: 70%;
-        background-color: green;
-    `}
-    
-    /* Styles for large screens */
-    ${media.greaterThan("large")`
-        height: 100%;
-        width: 100%;
-        background-color: green;
+
     `}
 
+    /* Styles for large screens */
+    ${media.greaterThan("large")`
+
+    `}
 `;
 
 const Button = styled.button `
 
     /* Styles for small screens */
     ${media.lessThan("medium")`
-        position: relative;
-        display: flex;
-        height: 40px;
-        width: 80px;
-        border: 3px solid #040f3d;
-        bottom: 20px;
-        left: 140px;
-        color: #040f3d;
-        background-color: transparent;
+        display: none;
 
         h5 {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            top: .3em;
-            margin: 0 auto;
-            text-align: center;
+            display: none;
         }
 
     `}
@@ -148,17 +139,46 @@ const Button = styled.button `
             opacity: 0.5;
         }
     `}
-
-
-
 `;
+
+const HalfHalf = styled.div `
+    /* Styles for small screens */
+    ${media.lessThan("medium")`
+        position: relative;
+        height: 600px;
+        width: 100%;
+        padding: 1em;
+
+        h3 {
+            position: relative;
+            color: #040f3d;
+        }
+
+        p {
+            position: relative;
+        }
+
+    `}
+
+    /* Styles for medium screens */
+    ${media.between("medium", "large")`
+
+    `}
+
+    /* Styles for large screens */
+    ${media.greaterThan("large")`
+
+    `}
+
+
+`
 
 const Landing = () => (
 
 <LandingContainer>
 
     <Slide left>
-        <div className="half">
+        <Half>
             <Fade left delay={400}>
                 <h2>I'm</h2>
             </Fade>
@@ -166,16 +186,38 @@ const Landing = () => (
             <Fade right delay={600}>
                 <h1>Zac.</h1>
             </Fade>
-        </div>
+        </Half>
     </Slide>
 
     <Fade delay={1300}>
-        <Button><h5>go</h5></Button>
+        <Button>
+            <h5>go</h5>
+        </Button>
     </Fade>
 
-    <div className="half-half">
+    <HalfHalf>
+        <Fade delay={3000}>
+            <h3>I'm aspiring to be a Full-Stack Web Developer.</h3>
+        </Fade>
+        <Fade delay={3800}>
+            <p>
+                I started my Web Development journey towards the end of 2018 and have continued to love it ever since.
+                Starting off with basic HTML and CSS, than moving to JavaScript, jQuery and a plethora of different frameworks.
+            </p>
+        </Fade>
 
-    </div>
+        <Fade delay={5000}>
+        <h3>Keep it simple and keep it responsive.</h3>
+        </Fade>
+        <Fade delay={6800}>
+            <p>
+                I decided to build my portfolio using <a href="https://gatsbyjs.org">GatsbyJS</a>.<br></br>
+                Allowing me to implement and leverage the powers of <a href="https://reactjs.org">ReactJS</a> and other great packages. <br></br>
+                All while maintaning a fast, responsive and simple layout.
+            </p>
+        </Fade>
+
+    </HalfHalf>
 
 </LandingContainer>
 
