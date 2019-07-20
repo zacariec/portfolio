99afd72bebd481d510a79121ec6f7befc537aca3
Gatsby <zacariealancarr@gmail.com>
2019-07-20 15:05:13 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 
 import { Link } from 'gatsby'
+
+import css from '../components/global.css'
+
 import Landing from '../components/landing'
 import Github from '../components/github'
 import BWith from '../components/builtwith'
