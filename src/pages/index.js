b87c36837c816b1debad171db806e2210162d59e
Gatsby <zacariealancarr@gmail.com>
2019-07-21 15:33:13 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,10 @@ import React from "react"
 
 import { Link } from 'gatsby'
 
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+import styled from 'styled-components' 
+import media from 'styled-media-query'
 import css from '../components/global.css'
 
 import Landing from '../components/landing'
@@ -10,9 +14,49 @@ import BWith from '../components/builtwith'
 import SEO from "../components/seo"
 
 
+const Contact = styled.div `
+	position: fixed;
+	display: flex;
+	width: 100%;
+	justify-content: flex-end;
+	padding: 1em;
+	z-index: 99;
+
+	a {
+		color: #040f3d;
+	}
+
+	h6:hover {
+		opacity: 0.8;
+	}
+
+    /* Styles for small screens */
+    ${media.lessThan("medium")`
+
+    `}
+
+    /* Styles for medium screens */
+    ${media.between("medium", "large")`
+
+    `}
+
+    /* Styles for large screens */
+    ${media.greaterThan("large")`
+
+    `}
+
+`
+
 
 const IndexPage = () => (
 <div>
+	<Contact>
+		<ul>
+			<li>
+				<AnchorLink href="#contact"><h6>Contact</h6></AnchorLink>
+			</li>
+		</ul>
+	</Contact>
     <Landing />
     <BWith />
     <Github />
