0e0fa7507f2ceef6cdb4373be6e5ff8e901a15f7
Gatsby <zacariealancarr@gmail.com>
2019-07-21 15:32:06 +1000

    Update builtwith.js

--- src/components/builtwith.js
+++ src/components/builtwith.js
@@ -18,9 +18,9 @@ const BWContainer = styled.div `
     	display: block;
     	padding: .5em;
     	width: 100%;
-    	height: 200px;
-    	background-color: #4bffa5;
+    	height: 100%;
     	box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+		background-color: white;
 
     	h6 {
     		color: #040f3d;
@@ -35,6 +35,11 @@ const BWContainer = styled.div `
 
     /* Styles for large screens */
     ${media.greaterThan("large")`
+    	position: relative;
+    	display: flex;
+    	flex-direction: column;
+    	justify-content: center;
+    	padding: 1em;
 
     `}
 `
@@ -58,14 +63,21 @@ const TitleContainer = styled.div `
 
     /* Styles for large screens */
     ${media.greaterThan("large")`
-
+    	position: relative;
+    	display: flex;
+    	width: 100%;
+    	flex-direction: row;
+    	justify-content: center;
     `}
 `
 const IconContainer = styled.div `
     /* Styles for small screens */
     ${media.lessThan("medium")`
-		display: flex;
-		justify-content: space-evenly;
+    	position: relative;
+    	width: 100%;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		grid-template-rows: 1fr 1fr;
     `}
 
     /* Styles for medium screens */
@@ -75,14 +87,20 @@ const IconContainer = styled.div `
 
     /* Styles for large screens */
     ${media.greaterThan("large")`
-
+    	position: relative;
+    	width: 100%;
+  		display: grid;
+  		padding: 1em;
+  		grid-template-columns: 1fr 1fr 1fr;
+  		grid-template-rows: 1fr 1fr;
     `}
 `
 
-const SCContainer = styled.div `
+const ColumnContainer = styled.div `
     /* Styles for small screens */
     ${media.lessThan("medium")`
     	position: relative;
+    	margin: 1em;
 		display: flex;
 		flex-direction: column;
 		text-align: center;
@@ -95,71 +113,16 @@ const SCContainer = styled.div `
 
     /* Styles for large screens */
     ${media.greaterThan("large")`
-
-    `}
-`
-const GContainer = styled.div `
-    /* Styles for small screens */
-    ${media.lessThan("medium")`
     	position: relative;
-		display: flex;
-		flex-direction: column;
-		text-align: center;
-    `}
+    	display: flex;
+    	margin: 1em;
+    	flex-direction: column;
+    	text-align: center;
 
-    /* Styles for medium screens */
-    ${media.between("medium", "large")`
-
-    `}
-
-    /* Styles for large screens */
-    ${media.greaterThan("large")`
-
-    `}
-`
-
-const RContainer = styled.div `
-    /* Styles for small screens */
-    ${media.lessThan("medium")`
-    	position: relative;
-		display: flex;
-		flex-direction: column;
-		text-align: center;
-    `}
-
-    /* Styles for medium screens */
-    ${media.between("medium", "large")`
-
-    `}
-
-    /* Styles for large screens */
-    ${media.greaterThan("large")`
-
-    `}
-`
-
-const LContainer = styled.div `
-    /* Styles for small screens */
-    ${media.lessThan("medium")`
-    	position: relative;
-		display: flex;
-		flex-direction: column;
-		text-align: center;
-
-		h6 {
-			position: relative;
-			top: 7px;
-		}
-    `}
-
-    /* Styles for medium screens */
-    ${media.between("medium", "large")`
-
-    `}
-
-    /* Styles for large screens */
-    ${media.greaterThan("large")`
-
+    	:hover {
+    		transform: scale(1.2);
+    		opacity: 0.8;
+    	}
     `}
 `
 
@@ -178,6 +141,9 @@ const BWIcon = styled.img `
 
     /* Styles for large screens */
     ${media.greaterThan("large")`
+    	position: relative;
+    	width: 150px;
+    	height: 1t50px;
 
     `}
 `
@@ -191,39 +157,35 @@ const BuiltWith = () => (
     	</TitleContainer>
 
     	<IconContainer>
-    		<Fade left delay={500}>
-    			<SCContainer>
+    			<ColumnContainer>
     				<a href="https://styled-components.com">
 						<BWIcon src={SIcon} alt=""/>
 					</a>
 					<h6>Styled<br/>Components</h6>
-				</SCContainer>
-			</Fade>
+				</ColumnContainer>
 
-    		<Fade left delay={700}>
-    			<GContainer>
+    			<ColumnContainer>
     				<a href="https://gatsbyjs.org">
 						<BWIcon src={GIcon} alt=""/>
 					</a>
 					<h6>Gatsby</h6>
-				</GContainer>
-			</Fade>
+				</ColumnContainer>
 
-    		<Fade left delay={900}>
-    			<RContainer>
+    			<ColumnContainer>
     				<a href="https://reactjs.org">
 						<BWIcon src={RIcon} alt=""/>
 					</a>
 					<h6>React</h6>
-				</RContainer>
-			</Fade>
+				</ColumnContainer>
 
-    		<Fade left delay={1100}>
-    			<LContainer>
+
+    			<ColumnContainer className="grid-fix">
+    				<a href="#">
 					<BWIcon src={LIcon} alt=""/>
 					<h6>Love</h6>
-				</LContainer>
-			</Fade>
+					</a>
+				</ColumnContainer>
+
 		</IconContainer>
     </BWContainer>
 
