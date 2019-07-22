f59436bb631a286dd3f60520d9c88ffc8c602f84
Gatsby <zacariealancarr@gmail.com>
2019-07-22 14:41:50 +1000

    Update landing.js

--- src/components/landing.js
+++ src/components/landing.js
@@ -3,35 +3,26 @@ import React from "react"
 import styled from 'styled-components' 
 import media from 'styled-media-query'
 
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-
-import Slide  from 'react-reveal/Slide'
 import Fade from 'react-reveal/Fade'
-import Flash from 'react-reveal/Flash'
-
-import { Link } from "gatsby"
-
-
-import DIcon from '../images/down-icon.png'
-
-
-//color palette.
-//#dd7035 #bd937d #dec8ba #e8e6e7 #ffffff
 
 const LandingContainer = styled.div `
 
     /* Styles for small screens */
     ${media.lessThan("medium")`
         position: relative;
-        height: 1100px;
+        height: 100%;
         width: 100%;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
     `}
 
     /* Styles for medium screens */
     ${media.between("medium", "large")`
+        position: relative;
+        display: flex;
+        flex-direction: row;
         height: 100%;
-        width: 70%;
-        background-color: green;
+        width: 100%;
+        flex-wrap: wrap;
     `}
     
     /* Styles for large screens */
@@ -76,7 +67,25 @@ const Half = styled.div `
 
     /* Styles for medium screens */
     ${media.between("medium", "large")`
+        position: relative;
+        display: flex;
+        overflow-x: hidden;
+        justify-content: flex-start;
+        align-self: center;
+        align-items: center;
+        height: 800px;
+        width: 50%;
+        background-color: #4bffa5;
+        z-index: 1;
 
+        h1 {
+            font-size: 12em;
+            color: #040f3d;
+        }
+
+        h2 {
+            display: none;
+        }
     `}
 
     /* Styles for large screens */
@@ -87,7 +96,7 @@ const Half = styled.div `
         justify-content: flex-start;
         align-self: center;
         align-items: center;
-        height: 400px;
+        height: 800px;
         width: 50%;
         background-color: #4bffa5;
         z-index: 1;
@@ -117,7 +126,21 @@ const WhiteHalf = styled.div `
 
     /* Styles for medium screens */
     ${media.between("medium", "large")`
+        display: flex;
+        position: relative;
+        justify-content: flex-end;
+        align-items: center;
+        height: 800px;
+        padding: 1em;
+        width: 50%;
+        background-color: white;
 
+            h2 {
+                postition: absolute;
+                margin-left: 1em;
+                font-size: 6em;
+                color: #4bffa5;
+            }
     `}
 
     /* Styles for large screens */
@@ -149,6 +172,7 @@ const ContentContainer = styled.div `
         width: 100%;
         padding: 1em;
         background-color: white;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
 
         h3 {
             position: relative;
@@ -163,7 +187,20 @@ const ContentContainer = styled.div `
 
     /* Styles for medium screens */
     ${media.between("medium", "large")`
+        postition: relative;
+        display: flex;
+        flex-direction: column;
+        padding: 5em;
 
+        h3 {
+            position: relative;
+
+            color: #040f3d;
+        }
+
+        p {
+            position: relative;
+        }
     `}
 
     /* Styles for large screens */
@@ -182,11 +219,7 @@ const ContentContainer = styled.div `
         p {
             position: relative;
         }
-
-
     `}
-
-
 `
 
 const Landing = () => (
@@ -210,20 +243,20 @@ const Landing = () => (
         </Half>
 
     <ContentContainer>
-        <Fade delay={3000}>
+        <Fade delay={2000}>
             <h3>I'm aspiring to be a Full-Stack Web Developer.</h3>
         </Fade>
-        <Fade delay={3800}>
+        <Fade delay={2500}>
             <p>
                 I started my Web Development journey towards the end of 2018 and have continued to love it ever since.
                 Starting off with basic HTML and CSS, than moving to JavaScript, jQuery and a plethora of different frameworks.
             </p>
         </Fade>
 
-        <Fade delay={5000}>
+        <Fade delay={3400}>
         <h3>Keep it simple and keep it responsive.</h3>
         </Fade>
-        <Fade delay={6800}>
+        <Fade delay={3800}>
             <p>
                 I decided to build my portfolio using <a href="https://gatsbyjs.org">GatsbyJS</a>.<br></br>
                 Allowing me to implement and leverage the powers of <a href="https://reactjs.org">ReactJS</a> and other great packages. <br></br><br></br>
